671ef7cc42e9dca5db769dd72c1b35f8cd2076df
unknown <martimanuel912@gmail.com>
2025-02-27 02:27:11 -0300

    arreglo

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,77 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@php
+$configData = Helper::appClasses();
+$customizerHidden = 'customizer-hide';
+@endphp
+
+@extends('layouts/blankLayout')
+
+@section('title', 'Forgot Password')
+
+@section('page-style')
+{{-- Page Css files --}}
+<link rel="stylesheet" href="{{ asset(mix('assets/vendor/css/pages/page-auth.css')) }}">
+@endsection
+
+@section('content')
+<div class="authentication-wrapper authentication-cover">
+  <div class="authentication-inner row m-0">
+    <!-- /Left Text -->
+    <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center">
+      <div class="flex-row text-center mx-auto">
+        <img src="{{asset('assets/img/pages/forgot-password-'.$configData['style'].'.png')}}" alt="Auth Cover Bg color" width="520" class="img-fluid authentication-cover-img" data-app-light-img="pages/forgot-password-light.png" data-app-dark-img="pages/forgot-password-dark.png">
+        <div class="mx-auto">
+          <h3>No worries, we will send you instructions 👩🏻‍💻</h3>
+          <p>
+            We can help you reset your password and security info. First, enter <br> your email address and click on send reset link.
+          </p>
+        </div>
+      </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <!-- /Left Text -->
+    <!-- Forgot Password -->
+    <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg p-sm-5 p-4">
+      <div class="w-px-400 mx-auto">
+        <!-- Logo -->
+        <div class="app-brand justify-content-center mb-4">
+          <a href="{{url('/')}}" class="app-brand-link gap-2 mb-2">
+            <span class="app-brand-logo demo">@include('_partials.macros')</span>
+            <span class="app-brand-text demo h3 mb-0 fw-bold">{{config('variables.templateName')}}</span>
+          </a>
         </div>
+        <!-- /Logo -->
+        <h4 class="mb-2">Forgot Password? 🔒</h4>
+        <p class="mb-4">Enter your email and we'll send you instructions to reset your password</p>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        @if (session('status'))
+        <div class="mb-1 text-success">
+          {{ session('status') }}
         </div>
-    </form>
-</x-guest-layout>
+        @endif
+
+        <form id="formAuthentication" class="mb-3" action="{{ route('password.email') }}" method="POST">
+          @csrf
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="john@example.com" autofocus>
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+          </div>
+          <button type="submit" class="btn btn-primary d-grid w-100">Send Reset Link</button>
+        </form>
+        <div class="text-center">
+          @if (Route::has('login'))
+          <a href="{{ route('login') }}" class="d-flex align-items-center justify-content-center">
+            <i class="bx bx-chevron-left scaleX-n1-rtl"></i>
+            Back to login
+          </a>
+          @endif
+        </div>
+      </div>
+    </div>
+    <!-- /Forgot Password -->
+  </div>
+</div>
+@endsection
