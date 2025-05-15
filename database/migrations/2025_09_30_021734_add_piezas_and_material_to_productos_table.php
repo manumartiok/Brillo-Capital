2a87a123ae337e08bda8f88c9fed1060a1992187
manumartiok <martimanuel912@gmail.com>
2025-05-14 21:48:14 -0300

    Nuevo deploy

--- database/migrations/2025_09_30_021734_add_piezas_and_material_to_productos_table.php
+++ database/migrations/2025_09_30_021734_add_piezas_and_material_to_productos_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('productos', function (Blueprint $table) {
-            $table->unsignedBigInteger('piezas_id')->nullable(); // Permitir nulos
-        $table->foreign('piezas_id')->references('id')->on('piezas')->onDelete('set null');
+        // Schema::table('productos', function (Blueprint $table) {
+        //     $table->unsignedBigInteger('piezas_id')->nullable(); // Permitir nulos
+        // $table->foreign('piezas_id')->references('id')->on('piezas')->onDelete('set null');
         
-        $table->unsignedBigInteger('material_id')->nullable(); // Permitir nulos
-        $table->foreign('materials_id')->references('id')->on('material')->onDelete('set null');
-        });
+        // $table->unsignedBigInteger('material_id')->nullable(); // Permitir nulos
+        // $table->foreign('materials_id')->references('id')->on('material')->onDelete('set null');
+        // });
     }
 
     /**
